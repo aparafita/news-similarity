a87a62093c39beb7c4f6e3513254cd2f6140fde4
Álvaro Parafita <parafita.alvaro@gmail.com>
2016-01-26 02:40:07 +0100

    Design improvements on viz

--- viz/js/utils.js
+++ viz/js/utils.js
@@ -17,3 +17,29 @@ function isin(l, x) {
             return true;
     return false;
 }
+
+
+function isoDateToSP(date){
+  var a = date.substring(0, 4),
+      m = date.substring(5, 7),
+      d = date.substring(8, 10);
+
+  return d + "/" + m + "/" + a;
+}
+
+
+// function color(x){
+//   switch(x){
+//     case 0: x = '#1F83B4'; break;
+//     case 1: x = '#18A188'; break;
+//     case 2: x = '#54A338'; break;
+//     case 3: x = '#ADB828'; break;
+//     case 4: x = '#FFBD4C'; break;
+//     case 5: x = '#FF9C0E'; break;
+//     case 6: x = '#E75727'; break;
+//     case 7: x = '#C94D8C'; break;
+//     case 8: x = '#B446B3'; break;
+//     case 9: x = '#8061B4'; break;
+//   }
+//   return x == undefined ? '#CDC9C9' : x;
+// }
